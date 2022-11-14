5b42475722849da7beef1022b43582ff0599ab67
PNSyildiz <111272061+PNSyildiz@users.noreply.github.com>
2022-11-14 20:23:43 +0200

    Add files via upload

---
+++ Profile.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import './Profile.css';
+
+
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    <div classname="colz-icon">
+                    <a href='g'>
+                        <i className='fa fa-faceBook-square'></i>
+                    </a>
+                </div>
+            <div className='profile-details-role'>
+                    <a href='a'>
+                        <i className='fa fa-google-plus-square'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-youtbe-square'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-twitter'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-faceBook-square'></i>
+                    </a>
+
+                    </div>
+                    <a href='g'>
+                        <i className='fa fa-faceBook-square'></i>
+                    </a>
+                </div>
+            <div className='profile-details-role'>
+                    <a href='a'>
+                        <i className='fa fa-google-plus-square'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-youtbe-square'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-twitter'></i>
+                    </a>
+                    <a href='g'>
+                        <i className='fa fa-faceBook-square'></i>
+                    </a>
+                <span className='primary-text'>
+                    {""}
+                  <h2> Hello,I am </h2>  <span className="highlighted-text">  <h3>Pascoa🤗</h3>
+                    </span>
+            <div className='profile-details-role'>
+                <span className='primary-text'>
+                    {""}
+                    <h1>
+                       Welcome To My Portfolio Profile 😃😃
+                    </h1>
+                    
+                    <span className="profile-role-tagline">
+                        Enjoy Building Applications for a learning Experience.
+                        </span>
+                        </span>
+                    </div>
+                    <div className="profile-options">
+                        <button className="btn primary-btn">
+                        {""}
+                        HIRE ME{" "} 
+                        </button>
+                        <a href="PascoaSekgota.pdf" download="PascoaSekgota.pdf">
+                            <button className="btn highlighted-btn">Get Resume</button>
+                        </a>
+                    </div>
+                </span>
+                
+                        
+                        </div>
+                      
+                        <div/>
+                     </div>
+               
+           <div/>
+                </div>
+
+                <div className="profile-picture">
+                    <div className="profile-picture-background">
+                    <span className='primary-text'>
+                    {""}
+
+<div className='description'>
+
+                                
+       </div>
+       
+</span>
+                
+        </div>
+  </div>
+ 
+        
+        
+        <span className='primary-text'>
+                    {""}
+                    <h1>
+                       About Myself🥁🥁🥁
+                    </h1>
+                    
+                    <span className='profile-role-tagline'>
+                        What You Should know About me!
+                    
+                        <div className='container about-container'>
+                            <div className='about-me'>
+                                <div className='about-me-image'>
+                               
+
+                        <div className='About-content'>
+                            <div className='about-cars'>
+                                <article className='about_card'>
+                                    <h5> Education</h5>
+                                    <small > Diploma in Software development</small>
+                                </article>
+                                <article className='about_card'>
+                                    <h5> Developer Languages</h5>
+                                    <small >JavaScript, Html, PHP, Mysql, Java, and Python </small>
+                                </article>
+                                <article className='about_card'>
+                                    <h5> Experience</h5>
+                                    <small >Self-practice in software evelopment, no official work experience</small>
+                                </article>
+                                <article className='about_card'>
+                                    <h5> Skills</h5>
+                                    <small >Fast learner, Excerlent communication, Problem-solving, Analytical thinking. </small>
+                                </article>
+                                <article className='about_card'>
+                                    <h2> My Interest In Sovtech</h2>
+                                    <small >Sovtech has elite software development teams which makes it the best environment for growth as an aspiring develoer. They innovate cloud software to enterprise business across the globe.And Best of all is their Company valueslike #smile, #Adapt, #results, #Tech And Many More.😎😃</small>
+                                </article>
+                   
+                            </div>
+                            <a href='Contact' className='btn btn-primary'>Contact Me</a>
+                        </div>
+                            </div>
+                            <div className="profile-picture">
+                    <div className="profile-picture-background">
+                    <span className='primary-text'>
+            {""}
+
+       <div className='description'>
+       
+                                </div>
+                                </span>
+                        </div>
+       </div>
+       
+       </div>
+                        
+                        </div>
+                </span>         
+
+        </span>                
+
+</div>
+
+  )
+}
+
