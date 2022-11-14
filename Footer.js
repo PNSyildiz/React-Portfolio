563b5307807145b36831ffe125c5a22d9de19bd3
PNSyildiz <111272061+PNSyildiz@users.noreply.github.com>
2022-11-14 20:24:21 +0200

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='footer-container'>
+        <div className='footer-parent'>
+        <img src={ require('../../Footer/Footer.jpg').default} alt ='no internet connection'
+         />
+        </div>
+        </div>
+  )
+}
