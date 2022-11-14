134beedcf13519a714da9bdac18ad26cc10229cf
PNSyildiz <111272061+PNSyildiz@users.noreply.github.com>
2022-11-14 20:05:40 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './PortfolioContainer/Home/Home';
+import Profile from './PortfolioContainer/Home/Profile/Profile';
+import About from './PortfolioContainer/AboutMe/About'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Home/>
+    <Profile/>
+    <About/>
+    </div>
+  );
+}
+
+export default App;
